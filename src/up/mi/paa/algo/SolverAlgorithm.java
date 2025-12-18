0a2104f6170fe087ed9c5c8db08eec09305b0a52
kubeaux <nathan.eullaffroy@gmail.com>
2025-12-18 11:54:53 +0100

    Création interface d'algo de résolution

---
+++ src/up/mi/paa/algo/SolverAlgorithm.java
@@ -0,0 +1,32 @@
+package up.mi.paa.algo;
+
+import up.mi.paa.model.Network;
+
+/**
+ * Interface définissant le contrat pour les algo de résolution
+ * de problèmes d'affectation dans un réseau électrique
+ */
+public interface SolverAlgorithm {
+
+    /**
+     * Tente d'améliorer le réseau donné en optimisant les connexions
+     * @param le réseau à optimiser (sera modifié)
+     * @param nombre max d'itérations
+     * @return le coût final du réseau après optimisation
+     */
+    double resoudre(Network network, int iterations);
+
+    /**
+     * Retourne le nom de l'algo
+     * 
+     * @return nom de l'algo
+     */
+    String getNom();
+
+    /**
+     * Retourne une description de l'algo.
+     * 
+     * @return description
+     */
+    String getDesc();
+}
